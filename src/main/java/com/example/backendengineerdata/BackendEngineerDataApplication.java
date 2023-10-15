ac4f2ca05ad5eb93388e9c95c72de82e8e7e7597
Isuru <isurusajith0@gmail.com>
2023-10-15 08:30:13 +0530

    upload the project

---
+++ src/main/java/com/example/backendengineerdata/BackendEngineerDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendengineerdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineerDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEngineerDataApplication.class, args);
+	}
+
+}
