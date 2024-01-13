df2006e6159696b562c2e40f8abf6f14196860c8
Isuru <isurusajith0@gmail.com>
2024-01-13 12:29:30 +0530

    constructor injection

--- src/main/java/com/example/backendengineerdata/api/CompanyHttpController.java
+++ src/main/java/com/example/backendengineerdata/api/CompanyHttpController.java
@@ -1,5 +1,18 @@
 package com.example.backendengineerdata.api;
 
+import com.example.backendengineerdata.service.custom.CompanyService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/companies")
 public class CompanyHttpController {
 
+    private final CompanyService companyService;
+
+    public CompanyHttpController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
 }
